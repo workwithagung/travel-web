ea4fd4a5d8092147775b8f58cf6c2cc873a013f4
workwithagung <workwithagung@hotmail.com>
2022-11-04 14:35:22 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'travel-green': '#20B970',
+        'travel-orange': '#FF7B38',
+        'travel-slate': '#060620'
+      }
+    },
+  },
+  plugins: [],
+}
